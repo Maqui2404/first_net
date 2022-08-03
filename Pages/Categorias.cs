57e4f08d1cf4ea22b529dfa2187a650490ad92c4
MarcoF <marfimaqui@gmail.com>
2022-08-03 17:28:11 -0500

    Se termínó hasta categorias

---
+++ Pages/Categorias.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace PATENT_web.Pages;
+
+public class Categorias
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string autor { get; set; }
+    public int DisplayOrder { get; set; }
+    public DateTime CreatedDataTime { get; set; } = DateTime.Now;
+
+}
---
+++ Pages/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace PATENT_web.Pages.Data;
+
+
+public class ApplicationDbContext :DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Categorias> Categories { get; set; }
+}
